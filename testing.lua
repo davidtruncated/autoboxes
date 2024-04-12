bacd7274ff3ea7fa8c3d190a8ed8d69b03ef86fd
petergriffin16 <117768818+petergriffin16@users.noreply.github.com>
2024-04-12 15:49:26 -0400

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -17,8 +17,9 @@ while wait(.5) do
             me:MoveTo(clovers[1].Position)
             fireproximityprompt(clovers[1]:FindFirstChild("ProximityPrompt")) 
         end
-        wait(0.3)
+        wait(0.1)
     end
+    wait(0.5)
     for x,y in pairs(game.Workspace:GetDescendants()) do
         if y.name == "Owner" then
             if y.Value == "midstacks" then
