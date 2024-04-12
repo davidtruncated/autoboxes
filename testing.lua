1faffcf7d8d565fc2322fb42b8bbbf13f4b4e855
petergriffin16 <117768818+petergriffin16@users.noreply.github.com>
2024-04-12 00:07:08 -0400

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -5,7 +5,7 @@ while wait(.5) do
     while #boxes ~= 0 do
         boxes = game.Workspace.Boxes:GetChildren()
         if #boxes > 0 then
-            me.CFrame = (boxes[1].CFrame)
+            me:MoveTo(boxes[1].Position-Vector3.new(0,0,5))
             wait(0.1)
         end
     end
@@ -13,13 +13,9 @@ while wait(.5) do
     while #clovers ~= 0 do
         clovers = game.Workspace.Clovers:GetChildren()
         if #clovers > 0 then
-        if clovers[1] then
             me:MoveTo(clovers[1].Position)
             wait(0.2)
-            if clovers[1] then
-                fireproximityprompt(clovers[1]:FindFirstChild("ProximityPrompt")) 
-            end
-        end
+            fireproximityprompt(clovers[1]:FindFirstChild("ProximityPrompt")) 
         end
         wait(0.3)
     end
