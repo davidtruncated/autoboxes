3149acf228731e7493a5a8945259f0ad47275abe
petergriffin16 <117768818+petergriffin16@users.noreply.github.com>
2024-04-12 00:20:30 -0400

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -5,8 +5,6 @@ while wait(.5) do
     while #boxes ~= 0 do
         boxes = game.Workspace.Boxes:GetChildren()
         if #boxes > 0 then
-            me.Position = boxes[1].Position
-            wait(0.1)
             me:MoveTo(boxes[1].Position)
         end
     end
