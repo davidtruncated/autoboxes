d5fc9db3422cccf3cfdbfa6d3cfa6be0e78ced78
petergriffin16 <117768818+petergriffin16@users.noreply.github.com>
2024-04-11 23:09:20 -0400

    Add files via upload

---
+++ testing.lua
@@ -0,0 +1,22 @@
+me = game.Players.LocalPlayer.Character
+while wait(.5) do
+    boxes = game.Workspace.Boxes:GetChildren()
+    clovers = game.Workspace.Clovers:GetChildren()
+    while #boxes ~= 0 do
+        boxes = game.Workspace.Boxes:GetChildren()
+        if #boxes > 0 then
+            me:MoveTo(boxes[1].Position)
+            wait(0.1)
+        end
+    end
+    wait(0.2)
+    me:MoveTo(game.Workspace.beamOrigin.Position)
+    while #clovers ~= 0 do
+       clovers = game.Workspace.Clovers:GetChildren()
+       if #clovers > 0 then
+       me:MoveTo(clovers[1].Position)
+       wait(0.1)
+       fireproximityprompt(clovers[1]:FindFirstChild("ProximityPrompt")) 
+       end
+    end
+end
