59490d335ac9e89f263a6b3898d345f8bf5948ef
petergriffin16 <117768818+petergriffin16@users.noreply.github.com>
2024-04-12 16:08:03 -0400

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -23,8 +23,10 @@ while wait(.5) do
         if y.name == "Owner" then
             if y.Value == "midstacks" then
                 me:MoveTo(y.Parent.Base.Position)
-                wait(2)
+                wait(1)
                 game.ReplicatedStorage.Rebirth:InvokeServer()
+                wait(4)
+                game.ReplicatedStorage.Layouts:InvokeServer("Load","Layout1")
                 wait(1)
             end
         end
