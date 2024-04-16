bcff44ede95f4860e5bba54123a71cee92a1ae77
petergriffin16 <117768818+petergriffin16@users.noreply.github.com>
2024-04-15 22:57:18 -0400

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -12,8 +12,8 @@ end]]--
 else
     chosen = "Layout2"
 end]]--
-game.workspace.Map:FindFirstChild("Rocks"):Destroy()
-game.workspace.Map:FindFirstChild("Trees"):Destroy()
+--game.workspace.Map:FindFirstChild("Rocks"):Destroy()
+--game.workspace.Map:FindFirstChild("Trees"):Destroy()
 
     --[[wait(0.4)
     while #clovers ~= 0 do
